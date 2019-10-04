b28ebb760a9f64243537b5e61bd7de561403447f
Yuriy Hulka <yhulka@ymail.com>
2019-10-04 17:19:41 +0300

    Updated to .NetStandart 2.1(.NetCore 3.0)

--- Wkhtmltopdf.NetCore/WkhtmltopdfConfiguration.cs
+++ Wkhtmltopdf.NetCore/WkhtmltopdfConfiguration.cs
@@ -1,21 +1,12 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Internal;
-using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.ObjectPool;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
+using System.Web.Mvc;
 
 namespace Wkhtmltopdf.NetCore
 {
@@ -59,18 +50,6 @@ namespace Wkhtmltopdf.NetCore
             services.TryAddTransient<IRazorViewToStringRenderer, RazorViewToStringRenderer>();
             services.TryAddTransient<IGeneratePdf, GeneratePdf>();
             services.TryAddSingleton(updateableFileProvider);
-
-            if (services.BuildServiceProvider().GetService<IControllerFactory>() == null)
-            {
-                var _hostingEnvironment = new HostingEnvironment();
-                services.TryAddSingleton<IHostingEnvironment>(_hostingEnvironment);
-                services.AddLogging();
-                services.AddMvc();
-            }
-            services.Configure<RazorViewEngineOptions>(options =>
-            {
-                options.FileProviders.Add(updateableFileProvider);
-            });
 
             return services;
         }
